275f22454f2600410034fe73cf8aadc2014a077b
Michael Mior <mmior@cs.rit.edu>
2018-09-17 11:09:51 -0400

    Read from ~/.pgpass

--- src/main/java/edu/rit/cs/mmior/SQLTest.java
+++ src/main/java/edu/rit/cs/mmior/SQLTest.java
@@ -1,24 +1,40 @@
 package edu.cs.rit.mmior;
 
-import java.io.Console;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 class SQLTest {
     public static void main(String args[]) {
-        Console console = System.console();
-        String username = console.readLine("Username: ");
-        String password = new String(console.readPassword("Password: "));
+        // Read the password file
+        // should be ~/.pgpass and look like
+        // reddwarf.cs.rit.edu:5432:USERNAME:USERNAME:PASSWORD
+        String homeDir = System.getProperty("user.home");
+        String content = null;
+
+        try {
+            content = new Scanner(new File(homeDir, ".pgpass")).useDelimiter("\\Z").next();
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not load password file.");
+        }
+
+        String parts[] = content.split(":");
+        String host = parts[0];
+        String port = parts[1];
+        String username = parts[2];
+        String password = parts[4];
 
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
 
         try {
-            con = DriverManager.getConnection("jdbc:postgresql://reddwarf.cs.rit.edu/", username, password);
+            con = DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + "/", username, password);
             stmt = con.createStatement();
             rs = stmt.executeQuery("SELECT sales_city FROM foodmart.region WHERE sales_city != 'None';");
             while (rs.next()) {
